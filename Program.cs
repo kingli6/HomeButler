a845583f5b01ed7a50ff27d389af1ffbcc665ea2
Jonathan Fisher <jonathan.fshr@gmail.com>
2021-09-22 15:26:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeButler1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cc = new ConsoleCompanion();
+        }
+    }
+}
